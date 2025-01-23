79544970502b77866dfa31e59564d40c88d8d541
WolfXLP <05.luque.x@gmail.com>
2025-01-23 18:48:43 +0100

    solucion todo

--- operaciones/index.js
+++ operaciones/index.js
@@ -1,9 +1,8 @@
 /**
  * Aquesta funcio suma tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
- * sdvsdvsvdsdv
  */
 function suma(...valors) {
     let resultat = 0;
@@ -16,16 +15,17 @@ function suma(...valors) {
 
 /**
  * Aquesta funcio multiplica tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
-function multiplica(...valors) { 
-    let resultat = 1; for (let valor of
-    valors) { resultat *= valor; } 
+function multiplica(...valors) {
+    let resultat = 1;
+    for (let valor of valors) {
+        resultat *= valor;
+    }
+    return resultat;
 }
-return resultat;
-
 
 module.exports.suma = suma;
 module.exports.multiplica = multiplica;
